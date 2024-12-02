0e70567954615ebc7d34ef5e73f3a99736804fed
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2024-12-02 19:21:04 -0300

    Adicionar serialização nos atributos nome e anoDeLancamento

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,7 +1,11 @@
 package br.com.alura.screenmatch.modelos;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Titulo implements Comparable<Titulo> {
+    @SerializedName("Title")
     private String nome;
+    @SerializedName("Year")
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
     private double somaAvaliacoes;
@@ -66,5 +70,10 @@ public class Titulo implements Comparable<Titulo> {
     public int compareTo(Titulo outroTitulo) {
         return this.getNome().compareTo(outroTitulo.getNome());
     }
-}
 
+    public String toString() {
+        return "Nome: "+nome +
+                "\n" +
+                "Ano de lançamento: "+ anoDeLancamento;
+    }
+}
