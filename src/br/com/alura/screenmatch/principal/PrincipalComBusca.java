68b70ffc08bf06d57c3b8db7492a8661971f7629
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2024-12-02 21:07:09 -0300

    Modificar a criação do GSON com política de nomenclatura, alterar nome do objeto TituloConvertido

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -1,7 +1,10 @@
 package br.com.alura.screenmatch.principal;
 
 import br.com.alura.screenmatch.modelos.Titulo;
+import br.com.alura.screenmatch.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
 import java.net.URI;
@@ -32,9 +35,12 @@ public class PrincipalComBusca {
         String json = response.body();
         System.out.println(json);
 
-        Gson gson = new Gson();
-        Titulo tituloConvertido = gson.fromJson(json, Titulo.class);
-        System.out.println(tituloConvertido);
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
 
+        TituloOmdb TituloOmdb = gson.fromJson(json, TituloOmdb.class);
+        System.out.println(TituloOmdb);
+        Titulo tituloConvertido = new Titulo(TituloOmdb);
+        System.out.println(tituloConvertido);
     }
 }
