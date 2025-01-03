8fc762a9a34be5d488de73b354d471a9ed2eb321
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-02 22:18:39 -0300

    Adicionar while de busca

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -18,54 +18,68 @@ public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
 
         Scanner teclado = new Scanner(System.in);
-        System.out.println("Qual filme quer buscar?: ");
-        String filme = teclado.nextLine();
-        filme = filme.replaceAll("\\s", "+");
 
-        String endereco = ("https://omdbapi.com/?t="+filme+"&apikey=7589c63");
-        try {
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(endereco))
-                    .build();
+        String busca = "";
+        while (!busca.equalsIgnoreCase("sair")) {
 
-            HttpResponse<String> response = client
-                    .send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println("Qual filme quer buscar?: ");
+            busca = teclado.nextLine();
 
-            String json = response.body();
-
-            Gson gson = new GsonBuilder()
-                    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
-
-            TituloOmdb TituloOmdb = gson.fromJson(json, TituloOmdb.class);
-            Titulo tituloConvertido = new Titulo(TituloOmdb);
-
-            FileWriter escrita = new FileWriter("D:\\Windows Sistema\\Documentos\\Filmes.txt");
-            escrita.write(tituloConvertido.toString());
-            escrita.close();
-
-            try {
-                File arquivo = new File("D:\\Windows Sistema\\Documentos\\Filmes.txt");
-                Scanner leitura =  new Scanner(arquivo);
-
-                while (leitura.hasNextLine()) {
-                    String texto = leitura.nextLine();
-                    System.out.println(texto);
-                }
-                leitura.close();
-            } catch(FileNotFoundException e) {
-                System.out.println("Não foi possível encontrar o arquivo. Verifique se ele existe e  se o nome está correto");
+            if (busca == "sair") {
+                break;
             }
 
-        } catch (NumberFormatException erro) {
-            System.out.println("Erro na conversão de texto para número");
+            if (busca.equalsIgnoreCase("sair")) {
+                break;
+            }
 
-        } catch (IllegalArgumentException erro) {
-            System.out.println("Erro de argumento na busca. Verifique o parâmetro que está sendo buscado");
-        } catch (ErroDeConversaoDeAnoException erro) {
-            System.out.println(erro.getMessage());
-        } finally {
-            System.out.println("Fim da execução");
+            busca = busca.replaceAll("\\s", "+");
+
+            String endereco = ("https://omdbapi.com/?t=" + busca + "&apikey=7589c63");
+            try {
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(endereco))
+                        .build();
+
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+
+                String json = response.body();
+
+                Gson gson = new GsonBuilder()
+                        .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+
+                TituloOmdb TituloOmdb = gson.fromJson(json, TituloOmdb.class);
+                Titulo tituloConvertido = new Titulo(TituloOmdb);
+
+                FileWriter escrita = new FileWriter("D:\\Windows Sistema\\Documentos\\Filmes.txt");
+                escrita.write(tituloConvertido.toString());
+                escrita.close();
+
+                try {
+                    File arquivo = new File("D:\\Windows Sistema\\Documentos\\Filmes.txt");
+                    Scanner leitura = new Scanner(arquivo);
+
+                    while (leitura.hasNextLine()) {
+                        String texto = leitura.nextLine();
+                        System.out.println(texto);
+                    }
+                    leitura.close();
+                } catch (FileNotFoundException e) {
+                    System.out.println("Não foi possível encontrar o arquivo. Verifique se ele existe e  se o nome está correto");
+                }
+
+            } catch (NumberFormatException erro) {
+                System.out.println("Erro na conversão de texto para número");
+
+            } catch (IllegalArgumentException erro) {
+                System.out.println("Erro de argumento na busca. Verifique o parâmetro que está sendo buscado");
+            } catch (ErroDeConversaoDeAnoException erro) {
+                System.out.println(erro.getMessage());
+            } finally {
+                System.out.println("Fim da execução");
+            }
         }
     }
 }
