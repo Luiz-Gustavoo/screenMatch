97095a1fa36c345499e60b8799bcd978865dc6bf
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-01-25 17:38:13 -0300

    Remover gson, adicionar o tituloConvertido na lista titulos, salvar a lista de titulos em um arquivo usando o objeto salvarTitulo

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -4,6 +4,7 @@ import br.com.alura.screenmatch.modelos.Titulo;
 import br.com.alura.screenmatch.modelos.TituloOmdb;
 import br.com.alura.screenmatch.services.ConsumirAPI;
 import br.com.alura.screenmatch.services.ConverteDados;
+import br.com.alura.screenmatch.services.EscreverArquivo;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -19,13 +20,7 @@ import java.util.Scanner;
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                .setPrettyPrinting()
-                .create();
-
         Scanner teclado = new Scanner(System.in);
-
         String busca = "";
         List<Titulo> titulos = new ArrayList<>();
 
@@ -43,18 +38,16 @@ public class PrincipalComBusca {
             String endereco = ("https://omdbapi.com/?t=" + busca + "&apikey=7589c63");
             ConsumirAPI consumirAPI = new ConsumirAPI();
             String retornoAPI = consumirAPI.consumirAPI(endereco);
-            System.out.println(retornoAPI);
 
             ConverteDados converteDados = new ConverteDados();
             TituloOmdb tituloOmdb = converteDados.converteDados(retornoAPI, TituloOmdb.class);
             Titulo tituloConvertido = new Titulo(tituloOmdb);
             System.out.println(tituloConvertido);
+            titulos.add(tituloConvertido);
 
         }
-
-        FileWriter escrita = new FileWriter("Filmes.txt");
-        escrita.write(gson.toJson(titulos));
-        escrita.close();
+        EscreverArquivo salvarTitulo = new EscreverArquivo();
+        salvarTitulo.escreverArquivo(titulos);
 
         try {
             File arquivo = new File("Filmes.txt");
